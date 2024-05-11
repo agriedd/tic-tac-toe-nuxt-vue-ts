9bae4215632d3e0a30d52c5ee58b2e1f51322f11
Agri Edd. Oematan <dnr739528@gmail.com>
2024-05-11 18:54:33 +0800

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
-  ssr: false,
   imports: {
     dirs: ["composables/**"],
   },
