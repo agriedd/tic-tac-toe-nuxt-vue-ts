51063c8c85b5e53b7020f57963436c4e100644c3
Agri Edd. Oematan <dnr739528@gmail.com>
2024-05-11 23:33:37 +0800

    nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
-const DRIVE_LETTER_REGEX = /^[a-z]:/i;
-
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
@@ -16,26 +13,11 @@ export default defineNuxtConfig({
     optimizeDeps: {
       include: ["howler"],
     },
-    build: {
-      rollupOptions: {
-        output: {
-          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-          sanitizeFileName(name) {
-            const match = DRIVE_LETTER_REGEX.exec(name);
-            const driveLetter = match ? match[0] : "";
-            return (
-              driveLetter +
-              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
-            );
-          },
-        },
-      },
-    },
   },
   nitro: {
     prerender: {
-      crawlLinks: true,
+      crawlLinks: false,
       failOnError: false,
-    }
+    },
   }
 });
