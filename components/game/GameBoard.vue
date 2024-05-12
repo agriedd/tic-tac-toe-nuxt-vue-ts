827b36234f78a8df9622098a823e73f6a9da2ed2
Agri Edd. Oematan <dnr739528@gmail.com>
2024-05-12 10:53:10 +0800

    improve color

--- components/game/GameBoard.vue
+++ components/game/GameBoard.vue
@@ -71,22 +71,21 @@ onMounted(()=>{
 	<div class="aspect-square grid grid-cols-3 grid-rows-3 gap-1">
 		<div v-for="(cell, i) in boards" :key="i" 
 			@click="drawCell({x: cell.x, y: cell.y})"
-			class="bg-slate-800 rounded-lg text-white flex flex-col justify-center items-center fill-slate-100 transition-all cursor-pointer hover:ring"
+			class="bg-slate-800 rounded-lg text-white flex flex-col justify-center items-center transition-all cursor-pointer hover:ring"
 			:class="[
 				{
-					'hover:ring-blue-800 active:bg-blue-900': playerTurn === 0 && cell.value === null && !cell.mark,
+					'hover:ring-blue-800 active:bg-blue-900 fill-blue-600': playerTurn === 0 && cell.value === null && !cell.mark,
 					'hover:ring-rose-800 active:bg-rose-900': playerTurn === 1 && cell.value === null && !cell.mark,
-					'hover:ring-blue-800': cell.value === 'o' && !cell.mark,
-					'hover:ring-rose-800': cell.value === 'x' && !cell.mark,
-					'!bg-blue-800 hover:ring-0': cell.value === 'o' && cell.mark,
-					'!bg-rose-800 hover:ring-0': cell.value === 'x' && cell.mark,
-					'!fill-slate-600 delay-200': cell.deprecated
+					'hover:ring-blue-800 fill-blue-600': cell.value === 'o' && !cell.mark,
+					'hover:ring-rose-800 fill-rose-600': cell.value === 'x' && !cell.mark,
+					'hover:ring-0 !bg-slate-900 fill-blue-600': cell.value === 'o' && cell.mark,
+					'hover:ring-0 !bg-slate-900 fill-red-600': cell.value === 'x' && cell.mark
 				}
 			]">
-			<div v-if="cell.value === 'x'">
+			<div v-if="cell.value === 'x'" :class="[{ 'opacity-30': cell.deprecated, 'animate-pulse': cell.mark }]">
 				<svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24" style=""><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
 			</div>
-			<div v-else-if="cell.value === 'o'">
+			<div v-else-if="cell.value === 'o'" :class="[{ 'opacity-30': cell.deprecated, 'animate-pulse': cell.mark }]">
 				<svg xmlns="http://www.w3.org/2000/svg" width="3.2rem" height="3.2rem" viewBox="0 0 24 24" style=""><path d="M12 2C6.486 2 2 6.486 2 12c.001 5.515 4.487 10.001 10 10.001 5.514 0 10-4.486 10.001-10.001 0-5.514-4.486-10-10.001-10zm0 18.001c-4.41 0-7.999-3.589-8-8.001 0-4.411 3.589-8 8-8 4.412 0 8.001 3.589 8.001 8-.001 4.412-3.59 8.001-8.001 8.001z"></path></svg>
 			</div>
 		</div>
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,10 @@ const playerTurnUpdated = (value: number) => {
 </script>
 <template>
 	<div class="bg-slate-950 min-h-screen text-white flex flex-col items-center">
-		<div class="max-w-sm w-full h-screen flex flex-col justify-center gap-2 p-2">
-			<div class="p-6 bg-slate-900 rounded-lg rotate-180 grow transition-all" :class="[
+		<div class="max-w-sm w-full h-screen flex flex-col justify-center gap-1 p-2">
+			<div class="p-6 bg-slate-900 bg-opacity-90 rounded-lg rotate-180 grow transition-all text-slate-600" :class="[
 				{
-					'ring ring-rose-400': playerTurn === 1
+					'ring-1 ring-rose-900 !text-white': playerTurn === 1
 				}
 			]">
 				Player 2
@@ -19,9 +19,9 @@ const playerTurnUpdated = (value: number) => {
 			<div>
 				<GameBoard @update:player-turn="playerTurnUpdated"></GameBoard>
 			</div>
-			<div class="p-6 bg-slate-900 rounded-lg grow transition-all" :class="[
+			<div class="p-6 bg-slate-900 bg-opacity-90 rounded-lg grow transition-all text-slate-600" :class="[
 				{
-					'ring ring-blue-400': playerTurn === 0
+					'ring-1 ring-blue-900 !text-white': playerTurn === 0
 				}
 			]">
 				Player 1
