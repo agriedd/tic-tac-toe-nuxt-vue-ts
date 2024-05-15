4df14bc953cc562dcac93ad98d07cb80a4b95c2e
Agri Edd. Oematan <dnr739528@gmail.com>
2024-05-15 15:53:07 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // wsBase: 'ws://localhost:4000'
-      wsBase: 'wss://games.gmitdiaspora.org'
+      wsBase: 'wss://games.gmitdiaspora.org:3000'
     }
   }
 });
